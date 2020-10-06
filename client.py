5345400ac1b3c09819981465e07ff7298b162274
German <gd.martinez@uniandes.edu.co>
2020-10-06 00:37:07 -0500

    Avances

--- client.py
+++ client.py
@@ -1,91 +1,73 @@
-# IMPORTS
-import socket  # Import socket module
+# Import socket module
+import socket
 import logging
-import datetime
 import hashlib
-
-# LOGGER
-# Create and configure logger
-logging.basicConfig(filename="clientLogger" + datetime.date.today().strftime("%B %d, %Y") + ".log",
-                    format='%(asctime)s %(message)s',
-                    filemode='w')
-# Creating an object
-logger = logging.getLogger()
-# Setting the threshold of logger to INFO
-logger.setLevel(logging.INFO)
-
-# PROTOCOLO
-# Creación del Socket: Puerto e IP
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
-host = '127.0.0.1'  # Ip address that the TCPServer  is there
-port = 65430  # Reserve a port for your service every new transfer wants a new port or you must wait.
-# 1. Conectarse al servidor TCP y mostrar que se ha realizado dicha conexión. Mostrar el estado de la conexión.
-host = socket.gethostname()  # Get local machine name
-s.connect((host, port))
-print('me conecto')
-# 2. Enviar notificación de preparado para recibir datos de parte del servidor.
-s.send("Cliente: Hello server!".encode())
-print('Hello')
-hashCalculado = ''
-hashRecibido = ''
+import time
+from _thread import *
+from threading import Thread
+import random
 
 
-# Calcular el nuevo Hash
-def getmd5file(archivo):
-    try:
-        hashmd5 = hashlib.md5()
-        with open(archivo, "rb") as f:
-            for bloque in iter(lambda: f.read(4096), b""):
-                hashmd5.update(bloque)
-        return hashmd5.hexdigest()
-    except Exception as e:
-        print("Error: %s" % (e))
-        return ""
-    except:
-        print("Error desconocido")
-        return ""
+def main():
+
+    cliente_num = random.random()*100000000000
+    # Initializes the client log
+    logging.basicConfig(filename="./files/client" + str(cliente_num) + ".log", level=logging.INFO,
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S'
+                        )
+    print('Bienvenido')
+    m = hashlib.sha256()
+    threaded(m, cliente_num)
 
 
-# 3. Recibir un archivo del servidor por medio de una comunicación a través de sockets TCP.
-# 3.5 Recibir el Hash
-for i in range(2):
-    size = s.recv(16).decode()  # Note that you limit your filename length to 255 bytes.
-    if not size:
-        break
-    size = int(size, 2)
-    filename = s.recv(size).decode()
-    filesize = s.recv(32).decode()
-    filesize = int(filesize, 2)
-    file_to_write = open(filename)
-    chunksize = 4096
-    while filesize > 0:
-        if filesize < chunksize:
-            chunksize = filesize
-        data = s.recv(chunksize)
-        file_to_write.write(data.decode())
-        filesize -= len(data)
+def threaded(m, cliente_num):
+    start_time = time.time()
 
-    if ('archivo' in filename):
-        hashCalculado = getmd5file(filename)
-    else:
-        hashRecibido = file_to_write.read()
-    print('File' + filename + ' received successfully')
-    file_to_write.close()
-# 4. Verificar la integridad del archivo con respeto a la información entregada por el servidor.
-print('Hash Calculado' + hashCalculado)
-print('Hash Recibido' + hashRecibido)
-if hashCalculado == hashRecibido:
-    print('Los Hash Coinciden: Archivo recibido correctamente')
-else:
-    print('Los Hash no Coinciden: Archivo recibido incorrectamente')
+    # Local host IP '127.0.0.1'
+    host = '127.0.0.1'
 
-# 5. Enviar notificación de recepción del archivo al servidor
-s.send("Cliente: Recibí el archivo!".encode())
-# 6. La aplicación debe permitir medir el tiempo de transferencia de un archivo en segundos.
-# Al final de cada transferencia la aplicación debe reportar si el archivo está completo y
-# correcto y el tiempo total de transferencia, para esto genere un log para cada intercambio de
-# datos entre cliente y servidor.
-# 7. Disponer un repositorio de los archivos recibidos y logs. (Revisar sección de recomendaciones).
+    # Define the port on which you want to connect
+    port = 50000
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.close()
-print('Client: connection closed')
+    # Connect to server on local computer
+    s.connect((host, port))
+    print("Cliente #%d Ready to receive info ", cliente_num)
+    logging.info("CLIENT: client #%d ready to receive info", cliente_num)
+    dataTotal = ''
+
+    while True:
+        # Message received from server
+        data = s.recv(1048576)
+        dataTotal += str(data.decode('ANSI'))
+
+        if not data:
+            print("Termino el envío")
+            break
+        elif (data.__contains__(b"HASHH")):
+            logging.info("Found hash")
+            index = data.find(b"HASHH")
+
+            index2 = dataTotal.find("HASHH")
+
+            m.update(dataTotal[:index2].encode(
+                encoding='ANSI', errors='strict'))
+
+            realM = data[index+5:]
+            print("Hash recibido ", realM.decode(
+                encoding='ANSI', errors='strict'))
+            print("Hash creado ", m.hexdigest())
+            if m.hexdigest() == realM.decode():
+                print("Hash correcto")
+                logging.info("CLIENT: hash correcto")
+            else:
+                print("Hash incorrecto")
+                logging.info("CLIENT: hash corrupto ")
+
+    logging.info('CLIENT: Tiempo del envío %s', (time.time()-start_time))
+    logging.info("---------------------------------------------")
+    s.close()
+
+
+main()
--- server.py
+++ server.py
@@ -1,124 +1,100 @@
-# IMPORTS
+# Import socket programming library
 import socket
-import os
+
+# Import thread module
+from _thread import *
+import threading
 import logging
-import datetime
 import hashlib
+import time
 
-HOST = '127.0.0.1'
-port = 65430
-
-newFile = ''
-path = ''
-# LOGGER
-# Create and configure logger
-logging.basicConfig(filename="serverLogger" + datetime.date.today().strftime("%B %d, %Y") + ".log",
-                    format='%(asctime)s %(message)s',
-                    filemode='w')
-# Creating an object
-logger = logging.getLogger()
-# Setting the threshold of logger to INFO
-logger.setLevel(logging.INFO)
-
-# INPUTS DE CONFIGURACIÓN
-while True:
-    # 2. Tener dos archivos disponibles para su envío a los clientes: un archivo de tamaño 100 MiB, y otro de 250 MiB. Se sugiera que uno de estos archivos sea multimedia.
-    # 3. La aplicación debe permitir seleccionar qué archivo desea enviarse a los clientes conectados y a cuántos clientes en simultáneo.
+# print_lock = threading.Lock()
+logging.basicConfig(filename="serverLog.log", level=logging.INFO,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S'
+                    )
 
 
-    selectedFile = input('Escoje un archivo')
+# Thread function
+# Socket is the socket object with which connection was made, used to send and receive messages
+def threaded(socket):
 
-    if selectedFile == '1':
-        newFile = '../videos/1.txt'
-        path = '../videos/'
-        break
-    elif selectedFile == '2':
-        newFile = '../videos/2.txt'
-        path = './archivos/'
-        break
+    while cont < num_conn:
+        pass
 
+    start_time = time.time()
+    m = hashlib.sha256()
+    logging.info("SERVER: iniciando")
+    print("El archivo que se va a abrir es: ", file)
+    logging.info("SERVER: el archivo abierto fue %s ", file)
+    with open(file, 'rb') as f:
 
-# Calcular el Hash
-def getmd5file(archivo):
-    try:
-        hashmd5 = hashlib.md5()
-        with open(archivo, "rb") as f:
-            for bloque in iter(lambda: f.read(4096), b""):
-                hashmd5.update(bloque)
-        return hashmd5.hexdigest()
-    except Exception as e:
-        print("Error: %s" % (e))
-        return ""
-    except:
-        print("Error desconocido")
-        return ""
+        # Data received from client
+        data = f.read()
 
+        m.update(data)
 
-hashCalculado = getmd5file(newFile)
-nombreHash = "hash" + selectedFile + ".txt"
-hashfile = open((path + nombreHash), "w+")
-if os.stat(path + nombreHash).st_size == 0:
-    hashfile.write(hashCalculado)
-hashfile.close()
+        # Send back reversed string to client
+        socket.send(data)
 
-pool = 0
-while True:
-    selectedPool = int(input('Select number of people to send the file (Max 25): '))
-    if selectedPool > 0 and selectedPool < 26:
-        pool = selectedPool
-        break
+        print("Fin de envío")
 
-print('Pool of ' + str(pool) + ' clients')
-print('Selected File to transfer: ' + newFile)
+        # Send hash
+        h = str(m.hexdigest())
+        print("Digest enviado: ", m.hexdigest())
+        socket.send(("HASHH" + h).encode(encoding='ANSI', errors='strict'))
 
-# PROTOCOLO
-# Creación del Socket: Puerto e IP
-# Reserve a port for your service every new transfer wants a new port or you must wait.
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
-host = socket.gethostname()  # Get local machine name
-s.bind((host, port))  # Bind to the port
-s.listen(selectedPool)  # Now wait for client connection.
-
-print('Server: listening....')
-
-poolCounter = 0
-while True:
-    # 1. Recibir conexiones TCP. La aplicación debe soportar 25 conexiones en simultáneo.
-    server, addr = s.accept()  # Establish connection with client.
-    poolCounter = poolCounter + 1
-    print('Server: Got connection from', addr)
-    print('Server: There are ' + str(poolCounter) + ' client(s) connected')
-    print('Server: You need ' + str(pool) + ' to send the file')
-    if poolCounter == pool:
-
-        # 4. Realizar la transferencia de archivos a los clientes definidos en la prueba.
-        directory = os.listdir(path)
-        for files in directory:
-            print(files)
-            filename = files
-            size = len(filename)
-            size = bin(size)[2:].zfill(16)  # encode filename size as 16 bit binary
-            server.send(size.encode())
-            server.send(filename.encode())
-
-            filename = os.path.join(path, filename)
-            filesize = os.path.getsize(filename)
-            filesize = bin(filesize)[2:].zfill(32)  # encode filesize as 32 bit binary
-            server.send(filesize.encode())
-
-            file_to_send = open(filename, 'rb')
-
-            l = file_to_send.read()
-            server.sendall(l)
-            file_to_send.close()
-            print('File Sent')
-
-# 6. La aplicación debe permitir medir el tiempo de transferencia de un archivo en segundos.
-# Al final de cada transferencia la aplicación debe reportar si el archivo está completo y
-# correcto y el tiempo total de transferencia, para esto genere un log para cada intercambio de
-# datos entre cliente y servidor.
-# 7. Disponer un repositorio de los archivos recibidos y logs. (Revisar sección de recomendaciones).
+ # Connection closed
+    socket.close()
 
 
-# server.close()
-#  break
+def main():
+
+    global file
+
+    # Initializes the server log
+    host = "localhost"
+    # Port number
+    port = 50000
+    logging.info('Connected to %s on port %s', host, port)
+
+    # Socket del servidor
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind((host, port))
+    print('Port %s', port)
+
+    # Become a server socket
+    serversocket.listen(5)
+    print("Socket listening")
+    inputText = int(input("\n ¿Qué archivo desea enviar?"
+                          "\n 1. 145 MB"
+                          "\n 2. 355 MB"))
+    if inputText == 1:
+        f = "../videos/Redes5G.mp4"
+    else:
+        f = "../videos/Vivaldi.mp4"
+
+    file = f
+    print("Archivo seleccionado: ", file)
+    global num_conn
+    num_conn = int(input('\n ¿A partir de cuántas conexiones desea enviar?'))
+    logging.info('SERVER: el número de conexiones definido es %s', num_conn)
+    print("Esperando conexiones...")
+
+    # Counter with number of connections received
+    global cont
+    cont = 0
+    while True:
+        # Accept connections from outside
+        (clientsocket, address) = serversocket.accept()
+        # lock acquired by client
+        # print_lock.acquire()
+        print('Connected to : ', address[0], ':', address[1])
+
+        # Start a new thread and return its identifier
+        start_new_thread(threaded, (clientsocket,))
+        cont += 1
+    serversocket.close()
+
+
+main()
